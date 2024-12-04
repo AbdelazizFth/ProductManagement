e1633da626e210a14162e12427d41d21196232b7
Abdelaziz FATHALLAH <azizfathallah50@gmail.com>
2024-12-04 14:31:07 +0100

    Ajoutez des fichiers projet.

---
+++ ProductManagement/ManageProducts.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductManagement
+{
+    public class ManageProducts
+    {
+        // Ajouter un produit à la liste des produits
+        public void AddProduct()
+        {
+
+        }
+
+        // Aficher la liste des produits
+        public void ShowAllProducts()
+        {
+
+        }
+
+        // Rechercher un produit dans la liste des produits
+        public void SearchProduct()
+        {
+
+        }
+
+        // Supprimer un produit de la liste des produits
+        public void RemoveProduct()
+        {
+
+        }
+
+
+        // Supprimer un produit de la liste des produits
+        public void ClearAllProduct()
+        {
+
+        }
+
+    }
+}
---
+++ ProductManagement/Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductManagement
+{
+    public class Product
+    {
+        public string ID { get; set; }
+        public string Name { get; set; }
+        public string Price { get; set; }
+
+        public override string ToString()
+        {
+            return $"ID : {ID}, Name : {Name}, Price : {Price}€ ";
+        }
+    }
+}
